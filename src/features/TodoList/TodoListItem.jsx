555c536596e766c195d7cc283ccd22c095e44844
kelliwisethebrave <198285550+kelliwisethebrave@users.noreply.github.com>
2026-05-08 22:57:31 -0600

    Add shared/TextInputWithLabel.jsx, update TodoForm and TodoListItem to handle TextInputWithLabel, add initial edit logic, extract validation for todo title to utils/todoValidation.js.

--- src/features/TodoList/TodoListItem.jsx
+++ src/features/TodoList/TodoListItem.jsx
@@ -1,15 +1,26 @@
+import { useState } from "react";
+import TextInputWithLabel from "../../shared/TextInputWithLabel.jsx";
+
 function TodoListItem({ todo, onCompleteTodo }) {
+  const [isEditing, setIsEditing] = useState(false);
   return (
     <li>
-      <input
-        type="checkbox"
-        checked={todo.isCompleted}
-        onChange={() => onCompleteTodo(todo.id)}
-      />
-      {/* set the `type` prop to "checkbox" */}
-      {/* add the `checked` props */}
-      {/* add `onChange` event listener that uses the `onCompleteTodo` helper` */}
-      {todo.title}
+      {isEditing ? (
+        <TextInputWithLabel value={todo.title} />
+      ) : (
+        <>
+          <input
+            type="checkbox"
+            id={`checkbox${todo.id}`}
+            checked={todo.isCompleted}
+            onChange={() => onCompleteTodo(todo.id)}
+          />
+          {/* set the `type` prop to "checkbox" */}
+          {/* add the `checked` props */}
+          {/* add `onChange` event listener that uses the `onCompleteTodo` helper` */}
+          <span onClick={() => setIsEditing(true)}>{todo.title}</span>
+        </>
+      )}
     </li>
   );
 }
