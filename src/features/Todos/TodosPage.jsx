c2679713f6d0c0a98b3adb73062a7b3ba534e6e3
kelliwisethebrave <198285550+kelliwisethebrave@users.noreply.github.com>
2026-05-24 17:39:22 -0600

    Update to complete first step of Part 5 - Add API Integration with useEffect - allowing tasks to be loaded from API.

--- src/features/Todos/TodosPage.jsx
+++ src/features/Todos/TodosPage.jsx
@@ -1,10 +1,48 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import TodoForm from "./TodoForm";
 import TodoList from "./TodoList/TodoList.jsx";
 import TodoListItem from "./TodoList/TodoListItem.jsx";
 
-function TodosPage() {
+function TodosPage({ token }) {
   const [todoList, setTodoList] = useState([]);
+  const [error, setError] = useState("");
+  const [isTodoListLoading, setIsTodoListLoading] = useState(false);
+
+  useEffect(() => {
+    async function fetchTodos() {
+      setIsTodoListLoading(true);
+      const options = {
+        method: "GET",
+        headers: {
+          "X-CSRF-TOKEN": token,
+        },
+        credentials: "include",
+      };
+      try {
+        const response = await fetch("api/tasks", options);
+        if (response.status === 401) {
+          throw new Error("Not authorized.");
+        }
+        if (!response.ok) {
+          throw new Error(
+            response.messge || "Failed to fetch todos from server",
+          );
+        }
+        const data = await response.json();
+        console.log(data);
+        setTodoList(data.tasks);
+      } catch (error) {
+        console.error(error);
+        setError(`Error: ${error.name} | ${error.message}`);
+      } finally {
+        setIsTodoListLoading(false);
+      }
+    }
+    if (token) {
+      fetchTodos();
+    }
+  }, [token]);
+
   function addTodo(todoTitle) {
     const newTodo = {
       //can be just title since they have the same name
